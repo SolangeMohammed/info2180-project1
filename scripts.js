699d1f8297a12778ddb3b6acb2b32509ef91e1e8
Solange Mohammed <solangemohammed123@gmail.com>
2021-10-16 17:13:02 -0400

    Made changes to css file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-/* Add your JavaScript to this file */
+// window.onload = function(){
+//     let form = document.querySelectorAll("#form-group");
+//     for(let i=0; i<form.length; i++){
+
+//         const submitMsg= document.getElementsByTagName('message');
+//         const submit = document.querySelector('.btn')
+//         submit.onclick= () =>{
+//             submitMsg.innerHTML= ""
+//             console.log('button clicked')
+//         }
+
+//     }
+    
+// }
