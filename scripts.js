6613f80b0827cbf5991b72cb535e60cd4bef4b7b
Solange Mohammed <solangemohammed123@gmail.com>
2021-10-18 17:17:23 -0400

    Added a file

--- scripts.js
+++ scripts.js
@@ -1,14 +1,23 @@
-// window.onload = function(){
-//     let form = document.querySelectorAll("#form-group");
-//     for(let i=0; i<form.length; i++){
+window.onload = function(){
+    let form = document.querySelectorAll("#form-group");
+    for(let i=0; i<form.length; i++){
 
-//         const submitMsg= document.getElementsByTagName('message');
-//         const submit = document.querySelector('.btn')
-//         submit.onclick= () =>{
-//             submitMsg.innerHTML= ""
-//             console.log('button clicked')
-//         }
+        const submitMsg= document.getElementsByTagName('message');
+        const submit = document.querySelector('.btn')
+        submit.onclick= () =>{
+            const email = document.getElementById('input email');
+            const emailName = document.getElementById('placeholder');
+            const emailMsg = document.getElementById("message");
+            if(email(email)){
+                emailMsg.innerHTML = "Thank you! Your email address" +emailName+ "has been added to our mailing list"
+            }else{
+                emailMsg.innerHTML= "Please enter a valid email address"
 
-//     }
+            }
+            
+            console.log('button clicked')
+        }
+
+    }
     
-// }
+}
