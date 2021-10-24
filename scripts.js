614f0ceded2ec07f8caa2287237174961ae841f9
Solange Mohammed <solangemohammed123@gmail.com>
2021-10-24 05:49:47 -0400

    Added changes to js file

--- scripts.js
+++ scripts.js
@@ -1,47 +1,44 @@
 
-function myFunction(){
-    let button = document.querySelector(".btn");
-    button.addEventListener('submit', (event) => {
+function click(){
+    const button = document.querySelector('.btn')
+     button.addEventListener('click', handleClick)
+
+    function handleClick(event){
         event.preventDefault();
-        console.log('button clicked!');
-        const email = document.getElementById("email").value.indexOf("@");
-        submitOK = "true";
-        const emailMsg = document.querySelector(".message")
+        const email = document.getElementById("email").value; 
+        const emailMsg = document.querySelector('.message');
         if (email == -1) {
             emailMsg.innerHTML = "Please enter a valid email address"
             console.log("Not a valid e-mail!");
-            submitOK = "false";
-        }
+                 }
         else{
-            emailMsg.innerHTML = "Thank you your email address :"+email+ "has been added to out mailing list!"
-            return false;
-
-        }
+            emailMsg.innerHTML = "Thank you your email address :" +email+ "  has been added to out mailing list!"
+            console.log("valid");
+            console.log(email);
+            
+             }
+            
         
-    })
+        }
+       
+            
+    
+    }
 
-}
-
-
-
-
-
-
-
+    
 
 
 
     
-//         const emailName = document.getElementById("email").placeholder;
-//             document.getElementById("message").innerHTML= emailName
-//             if(emailName != null ){
-//                 console.log(emailName)
-//                 emailName.innerHTML = "Thank you! Your email address" +emailName+ "has been added to our mailing list"
-//              }else{
-//                 emailName.innerHTML= "Please enter a valid email address"
 
-//             }         
-//             console.log('button clicked')
+
+
+
+
+
+
+
+
 
 
 
